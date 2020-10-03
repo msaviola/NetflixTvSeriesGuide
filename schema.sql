ca9511a0a36f073a84792f3251741e0d498403d7
Christine Ross <christinejoyross@gmail.com>
2020-10-03 13:36:04 -0700

    table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,14 @@
 
 CREATE DATABASE netflix;
 USE netflix;
+
+CREATE TABLE shows
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	genre varchar(255) NOT NULL,
+    duration INT NOT NULL,
+    mood varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
