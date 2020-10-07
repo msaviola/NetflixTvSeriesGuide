41e5686f8395a64576df65e1bb06d9965c5d4740
Christine Ross <christinejoyross@gmail.com>
2020-10-06 18:57:46 -0700

    seeds schema

---
+++ seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO shows (title, genre, seasons, mood, completed) 
+VALUES ('The Good Place', 'Comedy', 4, 'Cheerful', true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed) 
+VALUES ('Dexter', 'Drama', 8, 'Angry', true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed) 
+VALUES ('The Office', 'Comedy', 9, 'Cheerful', true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed) 
+VALUES ('13 Reasons Why', 'Drama', 4, 'Gloomy', true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed) 
+VALUES ('Black Mirror', 'Sci-Fi', 5, 'Excited', false);
+
+INSERT INTO shows (title, genre, seasons, mood, completed) 
+VALUES ('Bojack Horseman', 'Comedy', 6, 'Gloomy', true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed) 
+VALUES ('Stranger Things', 'Sci-Fi', 3, 'Excited', false);
+
+INSERT INTO shows (title, genre, seasons, mood, completed) 
+VALUES ('Unbreakable Kimmy Schmidt', 'Comedy', 4, 'Cheerful', true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed) 
+VALUES ('The Vampire Diraries', 'Drama', 8, 'Romantic', true);
+
+
