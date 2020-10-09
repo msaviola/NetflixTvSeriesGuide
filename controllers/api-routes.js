1d8d3849fd8ed12cb45139a8a1327970a7b9dd32
Christine Ross <christinejoyross@gmail.com>
2020-10-08 18:36:27 -0700

    routes

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,3 +1,45 @@
 module.exports = function (app) {
+    app.get("/api/shows", function(req, res){
+        db.Show.findAll({})
+          .then(function(dbShow){
+            res.json(dbShow);
+          })
+      });
     
+      app.post("/api/shows", function(req, res) {
+        console.log(req.body);
+        db.Show.create({
+          title: req.body.title,
+          genre: req.body.genre,
+          seasons: req.body.seasons,
+          mood: req.body.mood,
+          completed: req.body.completed,
+          length: req.body.length
+        })
+          .then(function(dbShow) {
+            res.json(dbShow);
+          });
+      });
+    
+      app.get("/api/shows/mood/:mood", function(req, res) {
+        db.Show.findAll({
+          where: {
+            mood: req.params.category
+          }
+        })
+          .then(function(dbShow) {
+            res.json(dbShow);
+          });
+      });
+    
+      app.get("/api/shows/length/:length", function(req, res) {
+        db.Show.findAll({
+          where: {
+            length: req.params.length //0 or 1
+          }
+        })
+          .then(function(dbShow) {
+            res.json(dbShow);
+          });
+      });
 };
--- server.js
+++ server.js
@@ -1,59 +1,34 @@
-<<<<<<< HEAD
 const express = require('express');
-const expressHandlebars = require('express-handlebars');
+// const expressHandlebars = require('express-handlebars');
+const Handlebars = require('handlebars')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+var db = require("./models");
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+ }));
 app.set('view engine', 'handlebars');
 
 require('./controllers/view-routes')(app);
 require('./controllers/api-routes')(app);
 
-app.listen(PORT, function() {
-    console.log('listening on port ' + PORT);
-});
-=======
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
-
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-// Requiring our models for syncing
- var db = require("./models");
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Static directory
-app.use(express.static("public"));
-
-// Routes
-// =============================================================
- require("./routes/html-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
-
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
   });
-});
->>>>>>> 17073aea15608c21b03cc6e7558e2addec36d50b
+
+// app.listen(PORT, function() {
+//     console.log('listening on port ' + PORT);
+// });
