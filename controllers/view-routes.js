0ec5adaea391dc0793d6b38c3ae4f23526dfd982
msaviola <67287151+msaviola@users.noreply.github.com>
2020-10-10 10:44:10 -0700

    christine attempt

--- controllers/view-routes.js
+++ controllers/view-routes.js
@@ -17,4 +17,15 @@ module.exports = function (app) {
         
     });
 
+    app.get("/api/shows/mood/:mood", function(req, res) {
+        db.Show.findAll({
+          where: {
+            mood: req.params.category
+          }
+        })
+          .then(function(dbShow) {
+            res.json(dbShow);
+          });
+      });
+
 };
