1d475936264f2cf15c0fe7984c4f2b0d99a63eed
Christine Ross <christinejoyross@gmail.com>
2020-10-13 20:20:28 -0700

    seed file show additions

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,6 +5,15 @@ INSERT INTO shows (title, genre, seasons, mood, completed, length)
 VALUES ('Dexter', 'Drama', 8, 'Angry', true, true);
 
 INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Breaking Bad', 'Drama', 5, 'Angry', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Hannibal', 'Drama', 3, 'Angry', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Van Helsing', 'Drama', 4, 'Angry', false, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
 VALUES ('The Office', 'Comedy', 9, 'Cheerful', true, false);
 
 INSERT INTO shows (title, genre, seasons, mood, completed, length) 
@@ -14,7 +23,37 @@ INSERT INTO shows (title, genre, seasons, mood, completed, length)
 VALUES ('Black Mirror', 'Sci-Fi', 5, 'Excited', false, true);
 
 INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Black Mirror', 'Sci-Fi', 5, 'Excited', false, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Attack on Titan', 'Anime', 1, 'Excited', false, false);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Star Trek', 'Sci-Fi', 5, 'Excited', false, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('The Flash', 'Sci-Fi', 6, 'Excited', false, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Supergirl', 'Sci-Fi', 5, 'Excited', false, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
 VALUES ('Bojack Horseman', 'Comedy', 6, 'Gloomy', true, false);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Unorthodox', 'Drama', 1, 'Gloomy', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Unbelievable', 'Drama', 1, 'Gloomy', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('The Killing', 'Drama', 4, 'Gloomy', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('The Fall', 'Drama', 3, 'Gloomy', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('The Sinner', 'Drama', 2, 'Gloomy', true, true);
 
 INSERT INTO shows (title, genre, seasons, mood, completed, length) 
 VALUES ('Stranger Things', 'Sci-Fi', 3, 'Excited', false, true);
@@ -25,5 +64,34 @@ VALUES ('Unbreakable Kimmy Schmidt', 'Comedy', 4, 'Cheerful', true, false);
 INSERT INTO shows (title, genre, seasons, mood, completed, length) 
 VALUES ('The Vampire Diraries', 'Drama', 8, 'Romantic', true, true);
 
--- INSERT INTO shows (title, genre, seasons, mood, completed, length) 
--- VALUES ('Outlander', 'Drama', );
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Outlander', 'Drama', 3, 'Romantic', false, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Gilmore Girls', 'Drama', 7, 'Romantic', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Gossip Girl', 'Drama', 6, 'Romantic', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Crazy Ex-Girlfriend', 'Comedy', 4, 'Romantic', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Boys Over Flowers', 'Drama', 1, 'Romantic', true, true);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ("Schitt's Creek", 'Comedy', 6, 'Cheerful', true, false);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('New Girl', 'Comedy', 7, 'Cheerful', true, false);
+
+INSERT INTO shows (title, genre, seasons, mood, completed, length) 
+VALUES ('Fuller House', 'Comedy', 5, 'Cheerful', true, false);
+
+
+
+
+
+
+
+
