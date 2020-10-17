ad8728989b0c4291800b2277c6cc3db2961b121d
msaviola <67287151+msaviola@users.noreply.github.com>
2020-10-17 14:52:20 -0700

    git production

--- config/config.js
+++ config/config.js
@@ -16,11 +16,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": "root",
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "port": 3306,
+    "username": process.env.DB_USERNAME ||"root",
+    "password": process.env.DB_PASSWORD ||"root",
+    "database": process.env.DB_DATABASE ||"netflix",
+    "host": process.env.DB_HOST ||"127.0.0.1",
+    "port": process.env.DB_PORT || 3306,
     "dialect": "mysql"
   }
 
